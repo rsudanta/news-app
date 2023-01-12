2a8e4f2bd37aba6a5f43389604ec1caf407d728a
Danta <rsudanta20@gmail.com>
2023-01-13 03:43:31 +0700

    handle no internet

--- app/src/main/java/com/rsudanta/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/rsudanta/newsapp/ui/NewsViewModel.kt
@@ -1,9 +1,15 @@
 package com.rsudanta.newsapp.ui
 
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.rsudanta.newsapp.NewsApplication
 import com.rsudanta.newsapp.models.Article
 import com.rsudanta.newsapp.models.News
 import com.rsudanta.newsapp.models.SearchHistory
@@ -13,14 +19,16 @@ import com.rsudanta.newsapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
+import java.io.IOException
 import javax.inject.Inject
 
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(
+    application: Application,
     private val newsRepository: NewsRepository,
     private val historyRepository: HistoryRepository
-) : ViewModel() {
+) : AndroidViewModel(application) {
     val breakingNews: MutableLiveData<Resource<News>> = MutableLiveData()
     val news: MutableLiveData<Resource<News>> = MutableLiveData()
     var categorizedNews: MutableLiveData<Resource<News>> = MutableLiveData()
@@ -41,24 +49,58 @@ class NewsViewModel @Inject constructor(
     private fun getBreakingNews() {
         viewModelScope.launch {
             breakingNews.postValue(Resource.Loading())
-            val response = newsRepository.getNews(pageNumber = 1, pageSize = 5)
-            breakingNews.postValue(handleNewsResponse(response))
+            try {
+                if (hasInternetConnection()) {
+                    val response = newsRepository.getNews(pageNumber = 1, pageSize = 5)
+                    breakingNews.postValue(handleNewsResponse(response))
+                } else {
+                    breakingNews.postValue(Resource.Error("No internet connection"))
+                }
+            } catch (t: Throwable) {
+                when (t) {
+                    is IOException -> breakingNews.postValue(Resource.Error("Network Failure"))
+                    else -> breakingNews.postValue(Resource.Error("Conversion Error"))
+                }
+            }
         }
     }
 
     private fun getNews() {
         viewModelScope.launch {
             news.postValue(Resource.Loading())
-            val response = newsRepository.getNews(pageNumber = 2)
-            news.postValue(handleNewsResponse(response))
+            try {
+                if (hasInternetConnection()) {
+                    val response = newsRepository.getNews(pageNumber = 2)
+                    news.postValue(handleNewsResponse(response))
+                } else {
+                    news.postValue(Resource.Error("No internet connection"))
+                }
+            } catch (t: Throwable) {
+                when (t) {
+                    is IOException -> news.postValue(Resource.Error("Network Failure"))
+                    else -> news.postValue(Resource.Error("Conversion Error"))
+                }
+            }
         }
     }
 
     fun getCategorizedNews(category: String) {
         viewModelScope.launch {
             categorizedNews.postValue(Resource.Loading())
-            val response = newsRepository.getNews(pageNumber = 1, category = category)
-            categorizedNews.postValue(handleNewsResponse(response))
+            try {
+                if (hasInternetConnection()) {
+                    val response = newsRepository.getNews(pageNumber = 1, category = category)
+                    categorizedNews.postValue(handleNewsResponse(response))
+                } else {
+                    categorizedNews.postValue(Resource.Error("No internet connection"))
+                }
+            } catch (t: Throwable) {
+                when (t) {
+                    is IOException -> categorizedNews.postValue(Resource.Error("Network Failure"))
+                    else -> categorizedNews.postValue(Resource.Error("Conversion Error"))
+                }
+            }
+
         }
     }
 
@@ -68,7 +110,6 @@ class NewsViewModel @Inject constructor(
                 return Resource.Success(resultResponse)
             }
         }
-        Log.e("err", response.toString())
         return Resource.Error(response.message())
     }
 
@@ -115,10 +156,38 @@ class NewsViewModel @Inject constructor(
         }
     }
 
-    fun deleteSavedArticle(url:String) {
+    fun deleteSavedArticle(url: String) {
         viewModelScope.launch {
             historyRepository.deleteSavedArticle(url)
         }
+    }
+
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager = getApplication<NewsApplication>().getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val activeNetwork = connectivityManager.activeNetwork ?: return false
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.activeNetworkInfo?.run {
+                return when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+        return false
     }
 
 }
